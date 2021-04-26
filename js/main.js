e3e067892f4374900959682b511a9535cacb3d76
javierascona <79239373+javierascona@users.noreply.github.com>
2021-04-26 18:20:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 
 
+
+
+$(document).on('click', '#btnm1', function() {
+  $('#m1').modal('show');
+});
+
+$(document).on('click', '#btnlink1', function() {
+  $('#m1').modal('hide');
+  $('#m1').modal('handleUpdate');
+  $('#m2').modal('show');
+});
+
+$(document).on('click', '#btnm2', function() {
+  $('#m2').modal('show');
+});
+
+$(document).on('click', '#btnlink2', function() {
+  $('#m2').modal('hide');
+  $('#m2').modal('handleUpdate');
+  $('#m1').modal('show');
+});
+
+
+function mostrarPassword() {
+  var cambio = document.getElementById("txtPassword");
+  if (cambio.type == "password") {
+    cambio.type = "text";
+    $('.icon').removeClass('bi bi-eye-slash').addClass('bi bi-eye');
+  } else {
+    cambio.type = "password";
+    $('.icon').removeClass('bi bi-eye').addClass('bi bi-eye-slash');
+  }
+}
+
+
+
+
   $(document).ready(function() {
     var owl = $('.owl-carousel');
     owl.owlCarousel({
